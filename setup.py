15fdcccfe756db81f6fbbc3b5d0905b24c45209c
AlissonLima19 <39962856+AlissonLima19@users.noreply.github.com>
2022-11-04 14:58:04 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_test",
+    name="packageDIO",
     version="0.0.1",
     author="alisson_lima",
     author_email="infoalisson21@gmail.com",
